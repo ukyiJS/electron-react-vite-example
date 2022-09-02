c021709f0b451353656dfbe11ece8a1f399d3e48
ukyi <devcat.js@gmail.com>
2022-09-02 18:14:19 +0900

    ✨ MainWindow 추가

--- src/main/modules/mainWindow.ts
+++ src/main/modules/mainWindow.ts
@@ -1,32 +1,39 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, BrowserWindowConstructorOptions, app } from 'electron';
 import path from 'path';
 import { container } from 'tsyringe';
 
-export class MainWindow extends BrowserWindow {
-  constructor() {
-    super({
+export class MainWindow {
+  private static mainWindow: MainWindow;
+
+  init = async () => {
+    const config: BrowserWindowConstructorOptions = {
       show: false,
       vibrancy: 'under-window',
       visualEffectState: 'active',
       webPreferences: {
-        preload: path.join(process.cwd(), 'dist', 'preload.js'),
+        nodeIntegration: false,
+        contextIsolation: true,
+        preload: path.join(app.getAppPath(), 'dist', 'preload.js'),
       },
-    });
-  }
-
-  static create = async () => {
-    const window = new MainWindow();
+    };
+    container.register(BrowserWindow, { useValue: new BrowserWindow(config) });
+    const mainWindow = container.resolve(BrowserWindow);
 
     const pageUrl = import.meta.env.DEV ? import.meta.env.VITE_DEV_SERVER_URL : new URL('dist/index.html', `file://${__dirname}`).toString();
-    window.loadURL(pageUrl).finally();
+    await mainWindow.loadURL(pageUrl);
+    mainWindow.once('ready-to-show', () => mainWindow.show());
 
-    window.on('closed', window.destroy);
-    window.once('ready-to-show', () => {
-      if (!window.isVisible()) window.show();
-      if (process.env.MODE === 'development') window.webContents.openDevTools();
-    });
+    return mainWindow;
+  };
 
-    container.register(MainWindow, { useValue: window });
-    return container.resolve(MainWindow);
+  static create = async () => {
+    if (!this.mainWindow) this.mainWindow = new MainWindow();
+
+    let window = BrowserWindow.getAllWindows().find(w => !w.isDestroyed());
+
+    if (!window) window = await this.mainWindow.init();
+    if (window.isMinimized()) window.restore();
+    if (!window.isVisible()) window.show();
+    window.focus();
   };
 }
