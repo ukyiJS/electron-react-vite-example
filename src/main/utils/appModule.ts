9501af9ddf2b401392a9c2440720b2c8a04b4516
ukyi <devcat.js@gmail.com>
2022-09-02 18:14:46 +0900

    ✨ appModule 추가

--- src/main/utils/appModule.ts
+++ src/main/utils/appModule.ts
@@ -1,7 +1,6 @@
 import { IPC_INVOKE, IPC_ON } from '@main/utils/decorators';
-import { ipcMain } from 'electron';
+import { BrowserWindow, ipcMain } from 'electron';
 import { container } from 'tsyringe';
-import { app, BrowserWindow } from 'electron';
 import InjectionToken from 'tsyringe/dist/typings/providers/injection-token';
 
 type Class<T = any> = new (...args: Array<any>) => T;
@@ -10,19 +9,17 @@ type Inject = {
   inject: any;
 };
 type Option = {
-  window(): Promise<BrowserWindow>;
   controllers: Class[];
   injects?: Inject[]
 };
 
-export const appModule = async ({ window, controllers, injects }: Option) => {
-  const mainWindow = await app.whenReady().then(window);
-
+export const appModule = async ({ controllers, injects }: Option) => {
   if (injects) injects.forEach(({ token, inject }) => container.register(token, { useValue: inject }));
 
   controllers.forEach(ControllerClass => {
     const controller = container.resolve(ControllerClass);
-    const controllerMethods = Object.getOwnPropertyNames(ControllerClass.prototype);
+
+    const controllerMethods = Object.getOwnPropertyNames(ControllerClass.prototype).filter(name => name !== 'constructor');
     controllerMethods.forEach(methodName => {
       const method = controller[methodName];
       const [invokeEvent, onEvent] = [IPC_INVOKE, IPC_ON].map(metadataKey => Reflect.getMetadata(metadataKey, controller, methodName));
@@ -32,7 +29,8 @@ export const appModule = async ({ window, controllers, injects }: Option) => {
       if (onEvent) {
         controller[methodName] = async (...args: any[]) => {
           const result = Reflect.apply(method, controller, args);
-          mainWindow.webContents.send(onEvent, result);
+          container.resolve(BrowserWindow).webContents?.send(onEvent, result);
+
           return result;
         };
       }
