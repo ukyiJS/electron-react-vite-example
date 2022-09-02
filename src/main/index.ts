51d2cf307ef5c2e121531f7e916b1764ff3111b4
ukyi <devcat.js@gmail.com>
2022-09-02 18:15:56 +0900

    ✨ initApp 추가

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,20 +1,48 @@
-import { app } from 'electron';
 import 'reflect-metadata';
+import { MainWindow } from '@main/modules/mainWindow';
+import { app, BrowserWindow } from 'electron';
+import { container } from 'tsyringe';
 import { AppController } from './app.controller';
-import { MainWindow } from './modules/MainWindow';
 import { appModule } from './utils/appModule';
 
-const initElectronApp = () => {
-  app.disableHardwareAcceleration();
-  app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
-  app.on('activate', MainWindow.create);
+const initApp = async () => {
+  app.on('window-all-closed', () => {
+    container.resolve(BrowserWindow).destroy();
+    if (process.platform !== 'darwin') app.quit();
+  });
+
+  app.on('second-instance', () => {
+    const mainWindow = container.resolve(BrowserWindow);
+    if (!mainWindow) return;
+
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  });
+
+  app.on('activate', () => {
+    const mainWindow = BrowserWindow.getAllWindows().find(w => !w.isDestroyed());
+    if (mainWindow) return mainWindow.focus();
+    return MainWindow.create();
+  });
+
+  const isSingleInstance = app.requestSingleInstanceLock();
+  if (!isSingleInstance) {
+    app.quit();
+    process.exit(0);
+  }
+
+  if (import.meta.env.DEV) {
+    app.whenReady()
+      .then(() => import('electron-debug').then(({ default: debug }) => debug({ showDevTools: false })))
+      .then(() => import('./modules/devTools').then(({ DevTools }) => DevTools.install(DevTools.REACT)));
+  }
+  await app.whenReady().then(MainWindow.create);
 };
 
 (async () => {
   try {
-    initElectronApp();
+    await initApp();
     await appModule({
-      window: MainWindow.create,
       controllers: [AppController],
     });
   } catch (e) {
