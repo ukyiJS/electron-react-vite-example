f7901c86eafc429de5f44076e1572abea0b4e6cb
ukyi <devcat.js@gmail.com>
2022-09-04 23:11:21 +0900

    🔧 publish 추가

--- electron-builder.js
+++ electron-builder.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-template-curly-in-string */
+require('dotenv/config');
 
 /** @type {import('electron-builder').Configuration} */
 const config = {
   productName: 'ukyi-app',
   appId: 'ukyi.app',
   files: ['dist/**/*'],
+  publish: [
+    {
+      provider: 'github',
+      owner: 'ukyiJS',
+      repo: 'electron-react-vite-template',
+      token: process.env.GH_TOKEN,
+    },
+  ],
   directories: {
     output: 'release/${version}',
   },
